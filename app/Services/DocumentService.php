74a24a31426e135e0c4675946170ff3d4ee80231
jlundstrom <github@jonl.us>
2016-11-17 14:06:58 -0500

    Added Document Searching

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+use Illuminate\Database\Eloquent\Model;
 use Storage;
 use Imagick;
 use DB;
@@ -107,6 +108,39 @@ class DocumentService
 	}
 
         return response()->json('File has been deleted');
+    }
+
+    public function searchDocument($search, $user)
+    {
+        $paramArray = array();
+        $strings = explode(' ', $search);
+        $stmt  = 'SELECT documents.* ';
+	$stmt .= 'FROM document_meta_tags ';
+	$stmt .= 'INNER JOIN documents ON document_meta_tags.document_id = documents.id ';
+	$stmt .= 'INNER JOIN users_groups ON users_groups.group_id = documents.group_id ';
+	$stmt .= 'INNER JOIN users ON users.id = users_groups.user_id ';
+	$stmt .= 'WHERE users.id = ? and ( ';
+	$paramArray[] = $user['id'];
+
+	foreach($strings as $val) {
+	    $stmt .= 'document_meta_tags.value like ? or ';
+	    $paramArray[] = '%'.$val.'%';
+	}
+        $stmt = substr($stmt, 0, -3);
+	$stmt .= ')';
+
+	#$doc = DB::table('document_meta_tags')
+	#    ->join('documents', 'document_meta_tags.document_id', '=', 'documents.id')
+	#    ->join('users_groups', 'users_groups.group_id', '=', 'documents.group_id')
+	#    ->join('users', 'users.id', '=', 'users_groups.users_id')
+	#    ->where('users.id', $user['id'])
+	#    ->whereIn('document_meta_tags.value', $strings)
+	#    ->get();
+
+	$docs = DB::select($stmt, $paramArray);
+	return response()->json($docs);
+
+	
     }
 
 }
